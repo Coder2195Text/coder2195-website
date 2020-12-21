abab0b0a1b54af798c17fffc9cc8484308360356
Glitch (coder2195) <none>
2020-12-21 16:04:49 +0000

    👬👠 Updated with Glitch

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,18 @@
-async function checkip() {
-  // ban anyone from entering while developing
-  let res = await fetch("https://www.cloudflare.com/cdn-cgi/trace");
-  let data = await res.text();
-  let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/;
-  let ip = data.match(ipRegex)[0];
-  ip = ip.toString();
-  // ban if IP doesnt match
-  if (ip !== "74.71.211.152") {
-    window.close();
-    // in case close() doesn't work
-    document.write("It's private! Coder2195 is developing!");
-    
+function checkip() {
+  function text(url) {
+    return fetch(url).then(res => res.text());
   }
+
+  text("https://www.cloudflare.com/cdn-cgi/trace").then(data => {
+    let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/;
+    let ip = data.match(ipRegex)[0];
+    console.log(ip);
+    ip = ip.toString();
+    // ban if IP doesnt match
+    if (ip !== "74.71.211.152") {
+      document.write("It's private! Coder2195 is developing!");
+    }
+  });
 }
 
 function setup() {
