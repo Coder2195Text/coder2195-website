1cd904ec566dbf080e055d1841e97c259f4d8281
Glitch (coder2195) <none>
2020-12-21 16:37:51 +0000

    ⛸✨ Updated with Glitch

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,19 @@
-function checkip() {
-  function text(url) {
-    return fetch(url).then(res => res.text());
+async function checkip() {
+  let res = await fetch("https://www.cloudflare.com/cdn-cgi/trace"); // blocks execution until fetch is finished
+  let data = await res.text();
+  let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/;
+  let ip = data.match(ipRegex)[0];
+  ip = ip.toString();
+  // ban if IP doesnt match
+  if (ip !== "74.71.211.152") {
+    document.write(
+      "<h1 style='font-size: 50pt; color: green; font-family: Verdana;'>It's private! Coder2195 is developing! Get out right this instant!</h1>"
+    );
   }
-
-  text("https://www.cloudflare.com/cdn-cgi/trace").then(data => {
-    let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/;
-    let ip = data.match(ipRegex)[0];
-    console.log(ip);
-    ip = ip.toString();
-    // ban if IP doesnt match
-    if (ip !== "74.71.211.152") {
-      document.write("It's private! Coder2195 is developing!");
-    }
-  });
 }
 
 function setup() {
+  $('body').css('visibility', 'visible')
   //hide certain elements
   $("#snap").css("display", "none");
   $("#python").css("display", "none");
