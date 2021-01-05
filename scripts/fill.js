a177ce02894d97ca70880f25f3dac9f4bee483c1
Coder2195Text <7487792-Coder2195Text@users.noreply.gitlab.com>
2021-01-05 18:28:01 +0000

    Update index.html, scripts/fill.js files

--- scripts/fill.js
+++ scripts/fill.js
@@ -95,12 +95,16 @@
     <img id="backdrop" src="../img/lines-of-code.jpg">
     <script type="module" src="../scripts/main.js"></script>
 */
-alert('hi')
-settings = document.createElement("div");
-settings.id = "settings"
-settingicon = document.createElement("img");
-settingicon.src = "../img/settings.png"
-settingicon.id = "setting-icon"
-settingicon.height="60px"
-settingicon.width="60px"
+$("body").add("div")
+.attr("id","settings");
+
+$("#settings").add("div")
+.attr("id","setting-icon")
+.attr("height","60px")
+.attr("width","60px")
+.attr("src","/img/settings.png");
+
+
+
+
 
