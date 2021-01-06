4fb11a79170e3b2a55f2825a7e9209d070df07f7
Coder2195Text <7487792-Coder2195Text@users.noreply.gitlab.com>
2021-01-06 17:35:05 +0000

    Update scripts/fill.js

--- scripts/fill.js
+++ scripts/fill.js
@@ -8,23 +8,24 @@ async function checkip() {
   queryallowed = false
   if (window.url.includes('?')){
     // there is a query
-    token = window.token
     var query
     query = window.location["href"]
     query = query.split('?') 
     query = query[1]
     eval(query)
-    if (token == "coder2195yes"){
+    if (window.token == "coder2195yes"){
       queryallowed = true
     }
   }
   // ban if IP doesnt match
   if (!(ip == "74.71.211.152" /*ipv4*/ || ip == "2603:7000" || queryallowed) /*ipv6*/) {
     console.log(ip);
+    alert('bye')
     document.write(
       "<h1 style='font-size: 50pt; color: green; font-family: Verdana;'>It's private! Coder2195 is developing! Get out right this instant!</h1>"
     );
   } else {
+    alert('hi')
     $('body').append(`
 <div id="settings">
       <img
