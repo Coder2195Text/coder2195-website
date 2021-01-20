26da86fec3bbb0a53bf2466c595a9b8f1bb65373
Coder2195Text <7487792-Coder2195Text@users.noreply.gitlab.com>
2021-01-20 19:18:13 +0000

    Update scripts/fill.js

--- scripts/fill.js
+++ scripts/fill.js
@@ -93,9 +93,6 @@ function fill(){
   `)
 }
 $('head').append(`
-<script>var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101297411);</script>
-<script async src="https://static.getclicky.com/js"></script>
-
 `)
 $.ajax({
   url: 'https://dotmaui.com/my-ip/raw/',
