b09dfb598d08bf88f54899cb5996f554f6dafe2e
Coder2195 <Coder2195Mail@gmail.com>
2023-01-16 14:22:38 -0500

    finalized

--- components/background/Background.tsx
+++ components/background/Background.tsx
@@ -9,28 +9,40 @@ import { PageProps } from "../../pages/_app";
 const Background: FC<PageProps> = ({ mounted }) => {
   if (!mounted) return <></>;
   const { resolvedTheme } = useTheme();
+  const [vantaEffect, setVantaEffect] = useState();
   useEffect(() => {
     const div = document.getElementById("bg");
-    if (div) div.innerHTML = "";
     //@ts-ignore
-    NET({
-      el: div,
-      //@ts-ignore
-      THREE,
-      mouseControls: true,
-      touchControls: true,
-      gyroControls: false,
-      minHeight: 200.0,
-      minWidth: 200.0,
-      scale: 0.5,
-      scaleMobile: 0.5,
-      color: resolvedTheme == "dark" ? 0xff3f81 : 0xedc852,
-      backgroundColor: resolvedTheme == "dark" ? 0x0 : 0xffffff,
-      points: 20.0,
-      maxDistance: 27.0,
-      spacing: 18.0,
-    });
-  });
+    if (!vantaEffect)
+      setVantaEffect(
+        NET({
+          el: div,
+          //@ts-ignore
+          THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1,
+          scaleMobile: 1,
+          color: resolvedTheme == "dark" ? 0xff3f81 : 0x6adc6a,
+          backgroundColor: resolvedTheme == "dark" ? 0x0 : 0xffffff,
+          points: 20.0,
+          maxDistance: 27.0,
+          spacing: 20.0,
+        })
+      );
+    else {
+      if (vantaEffect) {
+        //@ts-ignore
+        vantaEffect.setOptions({
+          color: resolvedTheme == "dark" ? 0xff3f81 : 0x6adc6a,
+          backgroundColor: resolvedTheme == "dark" ? 0x0 : 0xffffff,
+        });
+      }
+    }
+  }, [resolvedTheme]);
   return <></>;
 };
 
