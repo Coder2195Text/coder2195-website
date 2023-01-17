03ad0b5027ea20c815b533b2813ba9883f26c79d
Coder2195Text <coder2195mail@gmail.com>
2023-01-17 16:01:55 +0000

    optimzations for packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,70 +10,70 @@ import Background from "../components/background/Background";
 import Loading from "../components/loading/Loading";
 
 const AppBody: FC<{
-  children: JSX.Element | JSX.Element[];
-  mounted: boolean;
+	children: JSX.Element | JSX.Element[];
+	mounted: boolean;
 }> = ({ children, mounted }) => {
-  //if not mounted, just return the children which will already be handled against hydration mismatch
-  if (!mounted) return <>{children}</>;
-  const { resolvedTheme } = useTheme();
-  return (
-    <div
-      className={`scroll-smooth fixed w-screen h-[calc(100vh-72px)] overflow-auto px-3 py-4 bottom-0 ${resolvedTheme}`}
-    >
-      <NavBar />
-      <Background mounted={mounted} />
-      {children}
-    </div>
-  );
+	//if not mounted, just return the children which will already be handled against hydration mismatch
+	if (!mounted) return <>{children}</>;
+	const { resolvedTheme } = useTheme();
+	return (
+		<div
+			className={`scroll-smooth fixed w-screen h-[calc(100vh-5rem)] overflow-auto px-3 py-4 bottom-0 ${resolvedTheme}`}
+		>
+			<NavBar />
+			<Background mounted={mounted} />
+			{children}
+		</div>
+	);
 };
 
 export interface PageProps {
-  mounted: boolean;
+	mounted: boolean;
 }
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-  const [mounted, setMounted] = useState(false);
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+	const [mounted, setMounted] = useState(false);
+	useEffect(() => {
+		setMounted(true);
+	}, []);
 
-  return (
-    <ThemeProvider enableSystem={false}>
-      <DefaultSeo
-        title="Coder2195's - 404 Not Found"
-        additionalMetaTags={[
-          {
-            name: "msapplication-TileColor",
-            content: "#000000",
-          },
-          {
-            name: "theme-color",
-            content: "#000000",
-          },
-        ]}
-        openGraph={{
-          images: [
-            {
-              url: "https://coder2195.vercel.app/favicon.ico",
-              width: 256,
-              height: 256,
-              type: "image/png",
-            },
-          ],
-          siteName: "Coder2195's Website",
-        }}
-      />
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <AppBody mounted={mounted}>
-        <div id="bg" className={`fixed left-0 top-0 w-full h-full -z-10`}></div>
-        <Loading />
-        <Component {...pageProps} mounted={mounted} />
-      </AppBody>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider enableSystem={false}>
+			<DefaultSeo
+				title="Coder2195's - 404 Not Found"
+				additionalMetaTags={[
+					{
+						name: "msapplication-TileColor",
+						content: "#000000",
+					},
+					{
+						name: "theme-color",
+						content: "#000000",
+					},
+				]}
+				openGraph={{
+					images: [
+						{
+							url: "https://coder2195.vercel.app/favicon.ico",
+							width: 256,
+							height: 256,
+							type: "image/png",
+						},
+					],
+					siteName: "Coder2195's Website",
+				}}
+			/>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<AppBody mounted={mounted}>
+				<div id="bg" className={`fixed left-0 top-0 w-full h-full -z-10`}></div>
+				<Loading />
+				<Component {...pageProps} mounted={mounted} />
+			</AppBody>
+		</ThemeProvider>
+	);
 };
 
 export default App;
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -3,29 +3,39 @@ import { NextSeo } from "next-seo";
 import { FC } from "react";
 import { fetchBlogPreviews } from "../graphql/queries";
 import { IBlogPost } from "../graphql/types";
+import { PageProps } from "./_app";
 
-interface Props {
-  posts: Array<IBlogPost>;
+interface Props extends PageProps {
+	posts: Array<IBlogPost>;
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const posts = await fetchBlogPreviews();
-  return {
-    props: {
-      posts,
-    },
-    revalidate: 10,
-  };
+	const posts = await fetchBlogPreviews();
+	return {
+		props: {
+			posts,
+		},
+		revalidate: 10,
+	};
 };
 
-const Blog: FC<Props> = ({ posts }) => {
-  return (
-    <div>
-      <h3 id="title">Blog Posts</h3>
-      <br />
-      {JSON.stringify(posts)}
-    </div>
-  );
+const Blog: FC<Props> = ({ mounted, posts }) => {
+	const contents = (
+		<>
+			<h3 id="title">Blog Posts</h3>
+			<br />
+			{JSON.stringify(posts)}
+		</>
+	);
+	return (
+		<>
+			<NextSeo
+				title="Coder2195 - Home"
+				description="The home page of coder2195's website"
+			/>
+			{mounted ? contents : undefined}
+		</>
+	);
 };
 
 export default Blog;
